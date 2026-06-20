7575258e366a98596b5f564ace50c956ad68cea6
mrxbox1 <48248684+mrxbox1@users.noreply.github.com>
2026-06-20 16:23:24 +0200

    IT WORKED LET'S FUCKING GOOOOOOOOOO

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import pygame
 
+# some constants and shit
+WIDTH = 800
+HEIGHT = 600
+
 # initialize pygame
 pygame.init()
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Raycaster")
 
-# set the player position (in tiles)
-player_position = (1, 1)
+# set the player position and direction
+player_posX = 1.5
+player_posY = 1.5
+player_dirX = 1
+player_dirY = 0
+
+# set the camera plane
+plane_X = 0
+plane_Y = 0.66
 
 # create the tilemap
 tilemap = [[1,1,1,1,1,1,1,1],
@@ -26,15 +37,89 @@ while running:
             running = False
     
     window.fill((0, 0, 0))
+    
+    # the magic that i don't understand begins here
+    for x in range(WIDTH):
+        camera_X = 2 * x / WIDTH - 1
+        ray_dirX = player_dirX + plane_X * camera_X
+        ray_dirY = player_dirY + plane_Y * camera_X
 
-    # draw the tilemap
-    for row in range(len(tilemap)):
-        for tile in range(len(tilemap[row])):
-            if tilemap[row][tile] != 0:
-                _color = (0, 255, 0)
+        player_mapX = int(player_posX)
+        player_mapY = int(player_posY)
+
+        side_distX = 0
+        side_distY = 0
+        
+        if ray_dirX == 0: 
+            _x = 1e30
+        else:
+            _x = ray_dirX
+        if ray_dirY == 0:
+            _y = 1e30
+        else:
+            _y = ray_dirY
+
+        delta_distX = abs(1 / _x)
+        delta_distY = abs(1 / _y)
+        perp_wallDist = 0
+
+        step_X = 0
+        step_Y = 0
+
+        hit = 0
+        side = 0
+
+        if ray_dirX < 0:
+            step_X = -1
+            side_distX = (player_posX - player_mapX) * delta_distX
+        else:
+            step_X = 1
+            side_distX = (player_mapX + 1 - player_posX) * delta_distX
+        if ray_dirY < 0:
+            step_Y = -1
+            side_distY = (player_posY - player_mapY) * delta_distY
+        else:
+            step_Y = 1
+            side_distY = (player_mapY + 1 - player_posY) * delta_distY
+
+        while hit == 0:
+            if side_distX < side_distY:
+                side_distX += delta_distX
+                player_mapX += step_X
+                side = 0
             else:
-                _color = (64, 64, 64)
-            pygame.draw.rect(window, _color, [tile*8, row*8, 8, 8])
+                side_distY += delta_distY
+                player_mapY += step_Y
+                side = 1
+            if tilemap[player_mapY][player_mapX] > 0:
+                hit = 1
+
+        if side == 0:
+            perp_wallDist = (side_distX - delta_distX)
+        else:
+            perp_wallDist = (side_distY - delta_distY)
+        
+        if perp_wallDist == 0:
+            _p = 1e30
+        else:
+            _p = perp_wallDist
+        line_height = int(HEIGHT / _p)
+        
+        draw_start = -line_height / 2 + HEIGHT / 2
+        if draw_start < 0:
+            draw_start = 0
+        draw_end = line_height / 2 + HEIGHT / 2
+        if draw_end >= HEIGHT:
+            draw_end = HEIGHT - 1
+
+        color = (0, 0, 0)
+        if tilemap[player_mapY][player_mapX] == 1:
+            color = (0, 255, 0)
+
+        if side == 1:
+            color = (color[0] / 2, color[1] / 2, color[2] / 2)
+
+        pygame.draw.line(window, color, (x, draw_start), (x, draw_end))
 
     # update the display
     pygame.display.update()
